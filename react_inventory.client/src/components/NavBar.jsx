a50115bdd23fdb19c5eb8d59f28e81d331810ea1
Ciwot18 <ciro.vavotici@gmail.com>
2024-08-24 17:17:30 +0200

    Changed Icon to GatoPanzon

--- react_inventory.client/src/components/NavBar.jsx
+++ react_inventory.client/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { NavLink } from 'react-router-dom';
+import GatoPanzon from "./Shared/gato-panzon.jfif";
 
 const navigation = [
   { name: 'Dashboard', href: '/', current: true },
@@ -56,7 +57,7 @@ export default function NavBar() {
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">Open user menu</span>
-                  <img alt="Toggle user menu" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-8 w-8 rounded-full"/>
+                  <img alt="Toggle user menu" src={GatoPanzon} className="h-8 w-8 rounded-full"/>
                 </MenuButton>
               </div>
               <MenuItems transition className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
