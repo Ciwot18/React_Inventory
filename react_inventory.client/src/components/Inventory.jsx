44332deb40b3b986bb28cc1a1bb0a03cbbd2fd59
Ciwot18 <ciro.vavotici@gmail.com>
2024-08-26 11:26:32 +0200

    Rendering skeleton on loading

--- react_inventory.client/src/components/Inventory.jsx
+++ react_inventory.client/src/components/Inventory.jsx
@@ -1,20 +1,34 @@
-import InventoryItem from "./InventoryParts/InventoryItem";
+import { InventoryItem, InventoryItemSkeleton } from "./InventoryParts/InventoryItem";
 import { getInventoryItems } from "../helpers/InventoryItemApi";
+import { useState, useEffect } from "react";
 
 export default function Inventory() {
-    const inventoryItems = getInventoryItems();
+    const [inventoryItems, setInventoryItems] = useState(null);
+
+    useEffect(() => {
+        const fetchInventoryItems = async () => {
+            const items = await getInventoryItems();
+            setInventoryItems(items);
+        };
+        fetchInventoryItems();
+    }, []);
 
     return (
         <div className="bg-gray-100 px-4 min-h-[calc(100vh-64px)]">
             <h2 className="text-center pt-2 mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Elementi presenti nel tuo inventario</h2>
             <section className="bg-white dark:bg-gray-900 px-4">
                 <div className="gap-16 items-center py-2 px-4 mx-auto max-w-screen-xl lg:pb-4 lg:px-6">
-                    {inventoryItems.length > 0 ? (
+                    {(inventoryItems === null) ? (
                         <ul className="divide-y divide-gray-100">
-                        {inventoryItems.map((item, index) => (
-                            <InventoryItem key={index} item={item} />
-                        ))}
-                    </ul>
+                            <InventoryItemSkeleton />
+                            <InventoryItemSkeleton />
+                        </ul>
+                    ) : (inventoryItems.length > 0) ? (
+                        <ul className="divide-y divide-gray-100">
+                            {inventoryItems.map((item, index) => (
+                                <InventoryItem key={index} item={item} />
+                            ))}
+                        </ul>
                     ) : (
                         <p className="text-center text-xl text-gray-500 dark:text-gray-400">Non ci sono elementi presenti nel tuo inventario.</p>
                     )}
--- react_inventory.client/src/components/InventoryParts/InventoryItem.jsx
+++ react_inventory.client/src/components/InventoryParts/InventoryItem.jsx
@@ -1,6 +1,6 @@
 import { deleteInventoryItem } from "../../helpers/InventoryItemApi";
 
-export default function InventoryItem(props) {
+const InventoryItem = (props) => {
     const { id, name, description, category, status, quantity, imageURL } = props.item;
 
     return (
@@ -37,4 +37,26 @@ export default function InventoryItem(props) {
             </div>
         </li>
     )
-}
+};
+
+const InventoryItemSkeleton = () => {
+    return (
+        <li className="flex justify-between gap-x-6 py-5 animate-pulse">
+            <div className="flex min-w-0 gap-x-4">
+                <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                </svg>
+                <div className="min-w-0 flex-auto">
+                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                </div>
+            </div>
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24 my-1"></div>
+                <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24 my-1"></div>
+            </div>
+        </li>
+    )
+};
+
+export { InventoryItem, InventoryItemSkeleton };
