336ab43e63a1e0af109e7c4f8905ca17c8b9d2d2
Ciwot18 <ciro.vavotici@gmail.com>
2024-08-26 12:50:41 +0200

    Delete function with according Toast

--- react_inventory.client/src/components/Inventory.jsx
+++ react_inventory.client/src/components/Inventory.jsx
@@ -1,9 +1,14 @@
 import { InventoryItem, InventoryItemSkeleton } from "./InventoryParts/InventoryItem";
 import { getInventoryItems } from "../helpers/InventoryItemApi";
 import { useState, useEffect } from "react";
+import Toasts from "./Shared/Toasts";
+import { deleteInventoryItem } from "../helpers/InventoryItemApi";
 
 export default function Inventory() {
     const [inventoryItems, setInventoryItems] = useState(null);
+    const [toastState, setToastState] = useState(false);
+    const [toastType, setToastType] = useState(null);
+    const [toastMessage, setToastMessage] = useState(null);
 
     useEffect(() => {
         const fetchInventoryItems = async () => {
@@ -11,12 +16,27 @@ export default function Inventory() {
             setInventoryItems(items);
         };
         fetchInventoryItems();
-    }, []);
+    }, [inventoryItems]);
+
+    function deleteItem(id) {
+        deleteInventoryItem(id);
+        setToastState(true);
+        setToastType('success');
+        setToastMessage('Elemento rimosso con successo');
+        setTimeout(resetToast, 3000);
+    }
+
+
+    function resetToast() {
+        setToastState(false);
+        setToastType(null);
+        setToastMessage(null);
+    }
 
     return (
         <div className="bg-gray-100 px-4 min-h-[calc(100vh-64px)]">
             <h2 className="text-center pt-2 mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Elementi presenti nel tuo inventario</h2>
-            <section className="bg-white dark:bg-gray-900 px-4">
+            <section className="bg-white dark:bg-gray-900 px-4 max-h-[calc(0.75*(100vh-64px))] overflow-y-auto">
                 <div className="gap-16 items-center py-2 px-4 mx-auto max-w-screen-xl lg:pb-4 lg:px-6">
                     {(inventoryItems === null) ? (
                         <ul className="divide-y divide-gray-100">
@@ -26,7 +46,7 @@ export default function Inventory() {
                     ) : (inventoryItems.length > 0) ? (
                         <ul className="divide-y divide-gray-100">
                             {inventoryItems.map((item, index) => (
-                                <InventoryItem key={index} item={item} />
+                                <InventoryItem key={index} item={item} functions={{deleteItem}} />
                             ))}
                         </ul>
                     ) : (
@@ -34,6 +54,7 @@ export default function Inventory() {
                     )}
                 </div>
             </section>
+            <Toasts show={toastState} type={toastType} message={toastMessage} />
         </div>
     )
 }
